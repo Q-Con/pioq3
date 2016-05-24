f1bacea5a446262edd9e07d1ae417811894c7e4d
Daniel Porter <dan.porter@rehabstudio.com>
2016-05-25 00:22:39 +0100

    Port 005f870 to es code

--- code/es/es_input.c
+++ code/es/es_input.c
@@ -202,7 +202,7 @@ static void IN_ProcessEvents( void )
 
 	if (PeekMessage(&msg, NULL, 0, 0, PM_NOREMOVE)) {
 		if (!GetMessage(&msg, NULL, 0, 0))
-			Sys_Quit();
+			Cbuf_ExecuteText(EXEC_NOW, "quit Closed window\n");
 
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
