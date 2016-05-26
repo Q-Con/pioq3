3e7fd4f2d35e1ecc9837dc5cdb3dd958d4f53ed5
Daniel Porter <dan.porter@rehabstudio.com>
2016-05-26 18:18:04 +0100

    Add missing GLimp_Minimize definition

--- code/sdl/sdl_glimp.c
+++ code/sdl/sdl_glimp.c
@@ -224,6 +224,17 @@ void GLimp_Shutdown( void )
 
 /*
 ===============
+GLimp_Minimize
+
+Minimize the game so that user is back at the desktop
+===============
+*/
+void GLimp_Minimize( void )
+{
+}
+
+/*
+===============
 GLimp_LogComment
 ===============
 */
